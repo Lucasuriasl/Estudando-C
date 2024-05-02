f8b0b95d766a64dd8fc0c8a33cff4484dd8f2f75
Lucasuriasl <122507938+Lucasuriasl@users.noreply.github.com>
2024-05-02 22:16:51 +0100

    Add files via upload

---
+++ Variables_and_text.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int favNum = 90;
+    char characterName[] = "John";
+    int characterAge = "35";
+    printf("There once was a man named %s\n", characterName);
+
+    printf("he was 35 years old.\n");
+    printf("He really liked the name John\n");
+    printf("but did not like being %s.\n", characterAge);
+
+
+    printf("My favorite %s is %d", "number", favNum);
+
+
+
+
+    return 0;
+}
+
