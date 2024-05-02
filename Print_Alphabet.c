18b6ba2e7f64c5379b7b2ef6a347350b23f97670
Lucasuriasl <122507938+Lucasuriasl@users.noreply.github.com>
2024-05-02 22:09:18 +0100

    Add files via upload

---
+++ Print_Alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+    short c;
+
+    c = 96;
+    while (++c < 123);
+        write(1, &c, 1);
+}
+
+int main(void)
+{
+    ft_print_alphabet();
+    printf(c)
+
+}
