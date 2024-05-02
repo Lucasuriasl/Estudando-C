1123aa687d56245c4a0fcfce2aef0a9238f00031
Lucasuriasl <122507938+Lucasuriasl@users.noreply.github.com>
2024-05-02 22:11:55 +0100

    Add files via upload

---
+++ Note_to_lores.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*/
+
+ESSE PROGRAMA PRINTA UMA DECLARAÇÃO DE AMOR!
+
+/*/
+
+int main()
+{
+    printf("_____________________________\n");
+
+    printf("\n");
+    printf("Mozin,\n");
+    printf("\t eu te amo!");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\t\t\t s2");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("Assinado: Lucas Urias");
+    printf("\n");
+    printf("__________________________");
+    return 0;
+}
