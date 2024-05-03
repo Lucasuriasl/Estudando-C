0f9ea0e5c266a2cebf10457e70b66baf3c68f63f
Lucasuriasl <122507938+Lucasuriasl@users.noreply.github.com>
2024-05-03 17:58:16 +0100

    Update Print_Alphabet.c

--- Print_Alphabet.c
+++ Print_Alphabet.c
@@ -1,17 +1,28 @@
 #include <unistd.h>
 
-void ft_print_alphabet(void)
+void   ft_print_alphabet(void)
 {
     short c;
 
     c = 96;
-    while (++c < 123);
+    while (++c < 123)
+        write(1, &c, 1);
+}
+void    ft_print_alphabet2(void)
+{
+    short c;
+
+    c = '`';
+    while (++c <= 'z')
         write(1, &c, 1);
 }
 
 int main(void)
 {
     ft_print_alphabet();
-    printf(c)
-
+    write(1, "\n", 1);
+    ft_print_alphabet2();
+    write(1, "\n", 1);
 }
+
+
