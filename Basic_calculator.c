6752013b4640663a12c5ba374c57f8c55a50da15
Lucas Nascimento <122507938+Lucasuriasl@users.noreply.github.com>
2024-09-06 22:25:30 +0100

    Update Basic_calculator.c
    adjusting some small mistakes to make the program compile

--- Basic_calculator.c
+++ Basic_calculator.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    double num1;
-    double num2;
-    printf("Enter first number:\n");
-    scanf("%lf", &num1);
-    printf("Enter second number:\n");
-    scanf("lf", &num2);
+	double num1;
+	double num2;
+	printf("Enter first number:\n");
+	scanf("%lf", &num1);
+	printf("Enter second number:\n");
+	scanf("%lf", &num2);
 
-    printf("Answer %d", num1 + num2);
+	printf("Answer %f", num1 + num2);
 
-    return 0;
+	return 0;
 }
